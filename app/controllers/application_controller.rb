4041875b4b2d0fb4465cfec80bfd00d963dbd36a
ASHOK MAHARJAN <ashok997@gmail.com>
2019-08-27 02:39:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,9 @@ class ApplicationController < Sinatra::Base
     if @user
       session[:user_id] = @user.id
       redirect '/account'
-    
+    else
+      erb :index
+    end
   end
 
   get '/account' do
