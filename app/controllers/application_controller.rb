92481467f408b4d191499e81a72a71118d259ee1
ASHOK MAHARJAN <ashok997@gmail.com>
2019-08-27 02:33:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+     @user = User.find_by(username: params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
     
   end
 
